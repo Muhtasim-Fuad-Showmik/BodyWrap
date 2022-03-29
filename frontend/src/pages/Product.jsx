25e69fa9209638b799527491ffe5f5f450af0f04
Muhtasim Fuad Showmik <49075537+Muhtasim-Fuad-Showmik@users.noreply.github.com>
2022-03-29 10:44:16 +0600

    Finished designing the Product Details Page
    Fixed the scrollbar design on the reviews container of this page. Also added filter options and a counter for the number of products to add to the cart on this page.

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,5 +1,7 @@
 import "../starability-heartbeat.css";
 import styled from "styled-components";
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
@@ -50,6 +52,80 @@ const Ratings = styled.div`
     margin: 20px 0px;
 `
 
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+`
+
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`
+
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+    margin: 0px 5px;
+    cursor: pointer;
+`
+
+const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+
+`
+
+const FilterSizeOption = styled.option``
+
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+`
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid var(--red);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`
+
+const Button = styled.button`
+    padding: 15px;
+    border: 2px solid var(--maroonoak);
+    border-radius: 10px;
+    background-color: var(--deepclaret);
+    color: var(--lightpink);
+    cursor: pointer;
+    font-weight: 500;
+    transition: all 0.5s ease;
+
+    &:hover {
+        background-color: var(--rouge);
+        border: 2px solid var(--rouge);
+    }
+`
+
 const Description = styled.p`
     margin: 20px 0px;
 `
@@ -58,24 +134,7 @@ const ReviewContainer = styled.div`
     flex: 1;
     padding: 0px 10px;
     height: 450px;
-    overflow: auto;
     margin-right: 20px;
-
-    &:-webkit-scrollbar-track {
-        -webkit-box-shadow: none;
-        background-color: #F5F5F5;
-    }
-
-    &:-webkit-scrollbar {
-        width: 6px;
-        height: 6px;
-        background-color: #F5F5F5;
-    }
-
-    &:-webkit-scrollbar-thumb {
-        background-color: #337ab7;
-        border-radius: 10px;
-    }
 `
 
 const Review = styled.div`
@@ -122,10 +181,34 @@ const Product = () => {
             <InfoContainer>
                 <Title>Saree Mall Blazing Red Linen Blend Saree</Title>
                 <Price>989/-</Price>
-
                 <Ratings>
                     <p class="starability-result" data-rating="4"></p>
                 </Ratings>
+                <FilterContainer>
+                    <Filter>
+                        <FilterTitle>Color</FilterTitle>
+                        <FilterColor color="red"/>
+                        <FilterColor color="pink"/>
+                    </Filter>
+                    <Filter>
+                        <FilterTitle>Size</FilterTitle>
+                        <FilterSize>
+                            <FilterSizeOption>XS</FilterSizeOption>
+                            <FilterSizeOption>S</FilterSizeOption>
+                            <FilterSizeOption>M</FilterSizeOption>
+                            <FilterSizeOption>L</FilterSizeOption>
+                            <FilterSizeOption>XL</FilterSizeOption>
+                        </FilterSize>
+                    </Filter>
+                </FilterContainer>
+                <AddContainer>
+                    <AmountContainer>
+                        <RemoveIcon />
+                        <Amount>1</Amount>
+                        <AddIcon />
+                    </AmountContainer>
+                    <Button>ADD TO CART</Button>
+                </AddContainer>
 
                 <Description>
                     <p>100% Original Products</p>
@@ -176,7 +259,7 @@ const Product = () => {
             </InfoContainer>
         </Wrapper>
         <Wrapper> 
-            <ReviewContainer>
+            <ReviewContainer className="scrollbar">
                 <ReviewForm>
                     <RatingInput>
                         <h2>Rating:</h2>
