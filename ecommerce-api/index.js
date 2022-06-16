ec007c227bfa79efb5eaadd82ced20147a4e6fad
Muhtasim Fuad Showmik <49075537+Muhtasim-Fuad-Showmik@users.noreply.github.com>
2022-06-16 16:46:33 +0600

    Prepared a register endpoint for the api

--- ecommerce-api/index.js
+++ ecommerce-api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -11,7 +13,11 @@ mongoose.connect(process.env.MONGO_URL)
     console.log(err);
 });
 
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
 app.listen(process.env.PORT || 5000, ()=>{
     console.log(`Backend server is running...`);
 });
-
---
+++ ecommerce-api/routes/auth.js
@@ -0,0 +1,27 @@
+const User = require("../models/User");
+const router = require("express").Router();
+const CryptoJS = require("crypto-js");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+    });
+
+    try{
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
+
+});
+
+module.exports = router;
